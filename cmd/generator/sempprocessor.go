3aa35c0796ad22c1c8a971532f161ccb7b256667
Balazs Czoma <balazs.czoma@solace.com>
2024-06-19 11:00:49 -0400

    Fix [skip ci]

--- cmd/generator/sempprocessor.go
+++ cmd/generator/sempprocessor.go
@@ -83,7 +83,13 @@ func processSempResults(resourceTypeAndName string, attributes []*broker.Attribu
 				if len(valuesRes.(string)) > 0 {
 					systemProvisioned = isSystemProvisionedAttribute(valuesRes.(string))
 				}
-				val := "\"" + SanitizeHclStringValue(valuesRes.(string)) + "\""
+				escapedValue := SanitizeHclStringValue(valuesRes.(string))
+				var val
+				if !strings.Contains(escapedValue, "\"") {
+					val = "\"" + escapedValue + "\""
+				} else {
+					val = "\"\""
+				}
 				if reflect.TypeOf(attr.Default) != nil && fmt.Sprint(attr.Default) == fmt.Sprint(valuesRes) {
 					//attributes with default values will be added to the internal list but will be skipped from results
 					attributesWithDefaultValue[attr.TerraformName] = &val
